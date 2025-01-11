83fbe5e03168105adbad4ed2a1e82138aeaf5fb3
minorisakamoto <bu2321110020@bukkyo-u.ac.jp>
2025-01-11 06:38:11 +0000

    t

--- main.rb
+++ main.rb
@@ -3,9 +3,9 @@ puts '1.2024/1/11'
 puts '2.2025/1/11'
 puts '3.2025/7/10'
 
-kaitou = gets.to_i
+input = gets.chomp.to_i
 
-if kaitou == 2
+if input == 2
     puts "正解"
 else
     puts "不正解"
@@ -17,9 +17,23 @@ puts '1.2023/2/10'
 puts '2.2025/1/30'
 puts '3.2025/7/13'
 
-kaitou = gets.to_i
+input = gets.chomp.to_i
 
-if kaitou == 3
+if input == 3
+    puts "正解"
+else
+    puts "不正解"
+end
+
+
+puts "黄色い果物は？"
+puts "1.オレンジ"
+puts "2.りんご"
+puts "3.バナナ"
+
+input = gets.chomp.to_i
+
+if input == 3
     puts "正解"
 else
     puts "不正解"
--- main2.rb
+++ main2.rb
@@ -3,9 +3,9 @@ def qa(title, a1, a2, a3, num)
     puts a1
     puts a2
     puts a3
-    kaitou = gets.to_i
+input = gets.chomp.to_i
     
-    if kaitou == num.to_i
+    if input == num.to_i
         puts "正解"
     else
         puts "不正解"
@@ -31,17 +31,38 @@ def qa(data)
     puts data[:a1]
     puts data[:a2]
     puts data[:a3]
-    kaitou = gets.to_i
+input = gets.chomp.to_i
     
-    if kaitou == data[:num].to_i
+    if input == data[:num].to_i
         puts "正解"
     else
         puts "不正解"
     end
 end
 
-qal = {
-    title:"「今回のスクーリングが始まった日は？」"
-    a1: "1.2024/1/11"
-    a2: "2.2025/1/11"
+qa1 = {
+    title: "「今回のスクーリングが始まった日は？」",
+    a1: "1.2024/1/11",
+    a2: "2.2025/1/11",
+    a3: "3.2025/7/10",
+    num: 2
 }
+qa(qa1)
+
+qa2 = {
+    title: "「今回のスクーリングが終わった日は？」",
+    a1:'1.2023/2/10',
+    a2:'2.2025/1/30',
+    a3:'3.2025/7/13',
+    num: 3
+}
+qa(qa2)
+
+qa3 = {
+    title: "黄色い果物は？",
+    a1:"1.オレンジ",
+    a2:"2.りんご",
+    a3:"3.バナナ",
+    num: 3
+}
+qa(qa3)
